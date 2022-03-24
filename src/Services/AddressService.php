6a1e5726731355cbe9b876a7b1225152a2c140fe
James Wallen-Jones <j.wallen.jones@googlemail.com>
2022-03-24 16:30:54 +0000

    add apply discount endpoint

--- src/Services/AddressService.php
+++ src/Services/AddressService.php
@@ -19,4 +19,9 @@ class AddressService extends AbstractService
     {
         return $this->request('put', $this->buildPath('/addresses/%s', $id), $params, self::OBJECT_NAME);
     }
+
+    public function applyDiscount($id, array $params = []): Address
+    {
+        return $this->request('post', $this->buildPath('/addresses/%s/apply-discount', $id), $params, self::OBJECT_NAME);
+    }
 }
